7e88aae5c60e8baa2e94c52c24316e60af00c734
Raheel <raheelbtd@gmail.com>
2016-05-25 20:48:22 -0500

    added Making Log Messages code

--- GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -113,6 +114,7 @@ public class QuizActivity extends AppCompatActivity {
                 {
                     mCurrentIndex = 0;
                 }
+
                 else
                 {
                     mCurrentIndex = (mCurrentIndex+1)% mQuestionBank.length;
@@ -124,23 +126,32 @@ public class QuizActivity extends AppCompatActivity {
         updateQuestion();
     }
 
-    //optional
-  /*  @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_quiz, menu);
-        return true;
-    }
+    //Overriding more lifecycle methods
+    private static final String TAG = "QuizActivity";
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    } */
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
 }
